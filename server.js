877f41a40059b49953c338a775eb202d00f5c5be
Elodie Rafalimanana <rafalima@adobe.com>
2018-05-13 21:54:24 +0200

    Clean server.js

--- server.js
+++ server.js
@@ -5,36 +5,15 @@ const Rx = require('rxjs/Rx');
 const fs = require('fs');
 const renderHome = require('./renderHome');
 
-function homeData() {
-  const readFiles = Rx.Observable.bindNodeCallback(fs.readdir);
-
-  const homeData = readFiles('./public')
-  .flatMap(filenames => Rx.Observable.from(filenames))
-  .filter(filename => /\.png$/.test(filename))
-  .map(filename => ({
-      src: filename,
-      caption: filename.replace('.png', ''),
-      class: filename.replace(/(\.png)|\s+/g, '').toLowerCase()
-    })
-  )
-  .reduce((acc, curr) => [...acc, curr], [])
-
-  return homeData;
-}
+/**
+* usage: node server.js and go to http://localhost:3000
+* Run this if you want to have your page as a mini local website
+* where you can update the images
+*/
 
 app.use('/images', express.static(__dirname + '/images'));
 
 app.get('/', function (req, res) {
-  /*const subs = homeData().subscribe(images => {
-    const renderedHome = pug.renderFile('index.pug', {
-      title: 'Tool Presets Viewer',
-      subtitle: 'View how your Photoshop tool presets render',
-      images: images
-    });
-
-    res.send(renderedHome);
-    subs.unsubscribe();
-  });*/
 
   const subs = renderHome().subscribe(html => {
     res.send(html);
